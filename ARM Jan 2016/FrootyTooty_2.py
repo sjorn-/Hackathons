864975d4eec3e32908a919fb91bbec34186c90fd
sjorn- <sean.r.innes@googlemail.com>
2016-01-30 22:31:11 +0000

    Added comments to py file

--- ARM Jan 2016/FrootyTooty_2.py
+++ ARM Jan 2016/FrootyTooty_2.py
@@ -3,11 +3,11 @@ from pygame.mixer import Sound
 import RPi.GPIO as GPIO
 import time
 
-#pygame init#
+#pygame init.#
 pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.mixer.init()
 
-#Fruit Listed here with GPIO connectors#
+#Fruit Listed here with GPIO connectors.#
 #('name', port, Sound("file"), False, 0)#
 fruit = [('Apple',  26, Sound("./Horn 1.ogg"), False, 0),
          ('Orange', 21, Sound("./Horn 2.ogg"), False, 0),
@@ -16,14 +16,14 @@ fruit = [('Apple',  26, Sound("./Horn 1.ogg"), False, 0),
          ('Kiwi',   19, Sound("./Horn 5.ogg"), False, 0),
          ('Carrot', 13, Sound("./Horn 6.ogg"), False, 0)]
 
-#GPIO Setup#
+#GPIO Setup.#
 GPIO.setmode(GPIO.BCM)
 for x in fruit:
     GPIO.setup(x[1], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     x = (x[0], x[1], x[2], x[3], time.process_time())
 
 #Input loop#
-#Loops through the list of fruit, checks if being touched, plays sound if so#
+#Loops through the list of fruit, checks if being touched, plays sound if so.#
 while True:
     for x in range(0,6):
         curButtonState = GPIO.input(fruit[x][1])
