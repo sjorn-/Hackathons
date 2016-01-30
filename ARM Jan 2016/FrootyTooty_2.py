c4c4738e2d4a47c77a65a1865d121555fb89ca51
sjorn- <sean.r.innes@googlemail.com>
2016-01-30 22:54:29 +0000

    Fixed time bug and added sound directory

--- ARM Jan 2016/FrootyTooty_2.py
+++ ARM Jan 2016/FrootyTooty_2.py
@@ -9,18 +9,18 @@ pygame.mixer.init()
 
 #Fruit Listed here with GPIO connectors.#
 #('name', port, Sound("file"), False, 0)#
-fruit = [('Apple',  26, Sound("./Horn 1.ogg"), False, 0),
-         ('Orange', 21, Sound("./Horn 2.ogg"), False, 0),
-         ('Banana', 20, Sound("./Horn 3.ogg"), False, 0),
-         ('Tomato', 16, Sound("./Horn 4.ogg"), False, 0),
-         ('Kiwi',   19, Sound("./Horn 5.ogg"), False, 0),
-         ('Carrot', 13, Sound("./Horn 6.ogg"), False, 0)]
+fruit = [('Apple',  26, Sound("./sounds/Horn 1.ogg"), False, 0),
+         ('Orange', 21, Sound("./sounds/Horn 2.ogg"), False, 0),
+         ('Banana', 20, Sound("./sounds/Horn 3.ogg"), False, 0),
+         ('Tomato', 16, Sound("./sounds/Horn 4.ogg"), False, 0),
+         ('Kiwi',   19, Sound("./sounds/Horn 5.ogg"), False, 0),
+         ('Carrot', 13, Sound("./sounds/Horn 6.ogg"), False, 0)]
 
 #GPIO Setup.#
 GPIO.setmode(GPIO.BCM)
 for x in fruit:
     GPIO.setup(x[1], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    x = (x[0], x[1], x[2], x[3], time.process_time())
+    x = (x[0], x[1], x[2], x[3], time.time())
 
 #Input loop#
 #Loops through the list of fruit, checks if being touched, plays sound if so.#
@@ -30,8 +30,8 @@ while True:
         if (fruit[x][3] != curButtonState):
             if (curButtonState):
                 #print("x[4] = " + str(fruit[x][4]))
-                if (time.process_time() - fruit[x][4] > 0.2):
+                if (time.time() - fruit[x][4] > 0.2):
                     print(fruit[x][0] + " Pressed")
                     fruit[x][2].play()
-                    fruit[x] = (fruit[x][0], fruit[x][1], fruit[x][2], fruit[x][3], time.process_time())
+                    fruit[x] = (fruit[x][0], fruit[x][1], fruit[x][2], fruit[x][3], time.time())
         fruit[x] = (fruit[x][0], fruit[x][1], fruit[x][2], curButtonState, fruit[x][4])
