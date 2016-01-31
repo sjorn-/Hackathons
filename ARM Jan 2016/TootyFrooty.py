bcb2c3a972c9af7539d297652e329e3773da1b24
sjorn- <sean.r.innes@googlemail.com>
2016-01-31 01:09:08 +0000

    Switched from 5-tuples to lists.
    As lists can be altered more easily than the 5-tuples the code looks a little nicer like this. Before this I was unaware that a list in python could contain multiple different types.

--- ARM Jan 2016/TootyFrooty.py
+++ ARM Jan 2016/TootyFrooty.py
@@ -9,29 +9,27 @@ pygame.mixer.init()
 
 #Fruit Listed here with GPIO connectors.#
 #('name', port, Sound("file"), False, 0)#
-fruit = [('Apple',  26, Sound("./sounds/Horn 1.ogg"), False, 0),
-         ('Orange', 21, Sound("./sounds/Horn 2.ogg"), False, 0),
-         ('Banana', 20, Sound("./sounds/Horn 3.ogg"), False, 0),
-         ('Tomato', 16, Sound("./sounds/Horn 4.ogg"), False, 0),
-         ('Kiwi',   19, Sound("./sounds/Horn 5.ogg"), False, 0),
-         ('Carrot', 13, Sound("./sounds/Horn 6.ogg"), False, 0)]
+fruit = [['Apple',  26, Sound("./sounds/Horn 1.ogg"), False, 0],
+         ['Orange', 21, Sound("./sounds/Horn 2.ogg"), False, 0],
+         ['Banana', 20, Sound("./sounds/Horn 3.ogg"), False, 0],
+         ['Tomato', 16, Sound("./sounds/Horn 4.ogg"), False, 0],
+         ['Kiwi',   19, Sound("./sounds/Horn 5.ogg"), False, 0],
+         ['Carrot', 13, Sound("./sounds/Horn 6.ogg"), False, 0]]
 
 #GPIO Setup.#
 GPIO.setmode(GPIO.BCM)
 for x in fruit:
     GPIO.setup(x[1], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    x = (x[0], x[1], x[2], x[3], time.time())
 
 #Input loop#
 #Loops through the list of fruit, checks if being touched, plays sound if so.#
 while True:
-    for x in range(0,6):
-        curButtonState = GPIO.input(fruit[x][1])
-        if (fruit[x][3] != curButtonState):
+    for i in range(0,6):
+        curButtonState = GPIO.input(fruit[i][1])
+        if (fruit[i][3] != curButtonState):
             if (curButtonState):
-                #print("x[4] = " + str(fruit[x][4]))
-                if (time.time() - fruit[x][4] > 0.2):
-                    print(fruit[x][0] + " Pressed")
-                    fruit[x][2].play()
-                    fruit[x] = (fruit[x][0], fruit[x][1], fruit[x][2], fruit[x][3], time.time())
-        fruit[x] = (fruit[x][0], fruit[x][1], fruit[x][2], curButtonState, fruit[x][4])
+                if (time.time() - fruit[i][4] > 0.2):
+                    print(fruit[i][0] + " Pressed")
+                    fruit[i][2].play()
+                    fruit[i][4] = time.time()
+        fruit[i][3] = curButtonState
